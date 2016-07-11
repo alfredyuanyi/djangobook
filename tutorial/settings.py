fb2332474b7925f919e20aa6c06019c241a19251
qingtian <alfredyuanyi@gmail.com>
2016-07-11 17:56:15 +0800

    将管理界面的语言修改为中文

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #'django.middleware.csrf.CsrfResponseMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'tutorial.urls'
