ea861c8dffc97e451f486bd4f75825c6c9cfb12e
qingtian <alfredyuanyi@gmail.com>
2016-07-11 19:39:57 +0800

    修改import使引用看起来更简洁

--- djangobook/urls.py
+++ djangobook/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, patterns
-from djangobook.views import *
+from djangobook import views
 
 urlpatterns = [
-	url(r'^djangobook/hello$', Hello),
-	url(r'^$', Hello),
-	url(r'^djangobook/time/plus/(\d{1,2})/$', HoursAhead),
-	url(r'^djangobook/home/$', Home),
-	url(r'^djangobook/requestmeta/$', ShowRequestMeta),
+	url(r'^djangobook/hello$', views.Hello),
+	url(r'^$', views.Hello),
+	url(r'^djangobook/time/plus/(\d{1,2})/$', views.HoursAhead),
+	url(r'^djangobook/home/$', views.Home),
+	url(r'^djangobook/requestmeta/$', views.ShowRequestMeta),
 
 ]
